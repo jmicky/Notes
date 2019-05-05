cbe5aac1875babe3bea12e501097e4d36db58b72
jmicky30 <jmicky30@gmail.com>
2019-05-05 18:28:24 +0900

    一覧ページ、Editページの追加

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +7,14 @@ namespace Notes
 {
     public partial class App : Application
     {
+        public static string FolderPath { get; private set; }
+
         public App()
         {
             InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new NotesPage());
 
-            MainPage = new MainPage();
         }
 
         protected override void OnStart()
--- Notes/Notes/Models/Note.cs
+++ Notes/Notes/Models/Note.cs
@@ -6,7 +6,7 @@ namespace Notes.Models
 {
     public class Note
     {
-        public string FileName { get; set; }
+        public string Filename { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
     }
