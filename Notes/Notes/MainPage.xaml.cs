d2f7ef77bac26411eef0a390ecf1b4540089b66a
jmicky30 <jmicky30@gmail.com>
2019-05-05 16:27:45 +0900

    単一ページのメモ保存機能

--- Notes/Notes/MainPage.xaml.cs
+++ Notes/Notes/MainPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Notes
@@ -13,9 +10,30 @@ namespace Notes
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
         public MainPage()
         {
             InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
         }
     }
 }
